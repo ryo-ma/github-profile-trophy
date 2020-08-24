869c5281012f86716582ee572fd444a570910a64
ryo-ma <saka_ro@yahoo.co.jp>
2020-08-24 21:08:21 +0900

    Modify dev_main.ts as common

--- dev_main.ts
+++ dev_main.ts
@@ -1,34 +1,7 @@
 import { serve } from "https://deno.land/std@0.66.0/http/server.ts";
-import { GithubAPIClient } from "./src/github_api_client.ts";
-import { Card } from "./src/card.ts";
-import "https://deno.land/x/dotenv@v0.5.0/load.ts";
+import requestHandler from "./index.ts";
 
 const s = serve({ port: 8080 });
-const client = new GithubAPIClient();
-const cacheMaxAge = 7200;
 for await (const req of s) {
-  const username = new URLSearchParams(req.url.split("?")[1]).get("username");
-
-  if (username != null) {
-    const userInfo = await client.requestUserInfo(username);
-    req.respond(
-      {
-        body: new Card().render(userInfo),
-        headers: new Headers(
-          {
-            "Content-Type": "image/svg+xml",
-            "Cache-Control": `public, max-age: ${cacheMaxAge}`,
-          },
-        ),
-      },
-    );
-  } else {
-    req.respond(
-      {
-        body: "Can not find a query parameter: username",
-        status: 404,
-        headers: new Headers({ "Content-Type": "text" }),
-      },
-    );
-  }
+  requestHandler(req);
 }
