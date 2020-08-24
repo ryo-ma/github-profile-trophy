e17cfb9bb458f9c6c167aed45ce4512beed28d8e
ryo-ma <saka_ro@yahoo.co.jp>
2020-08-24 23:02:43 +0900

    Add abridgeScore

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,18 @@ export function parseParams(req: ServerRequest): URLSearchParams {
   return new URLSearchParams(splitedURL[1]);
 }
 
+export function abridgeScore(score: number): string {
+  if (score < 1) {
+    return "unknown";
+  }
+  if (Math.abs(score) > 999){
+    return (Math.sign(score) * (Math.abs(score) / 1000)).toFixed(1) + "k+"
+
+  }
+  return (Math.sign(score) * Math.abs(score)).toString().slice(0, -1) + "0+";
+
+}
+
 export const CONSTANTS = {
   CACHE_MAX_AGE: 7200,
   DEFALT_PANEL_SIZE: 110,
@@ -16,7 +28,9 @@ export const CONSTANTS = {
 };
 
 export enum RANK {
+  SP = "S+",
   S = "S",
+  AP = "A+",
   A = "A",
   B = "B",
   C = "C",
