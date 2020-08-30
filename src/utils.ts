f6a9b83b116bd06eab92bb5a0a515e44e8cc0901
ryo-ma <saka_ro@yahoo.co.jp>
2020-08-30 19:21:04 +0900

    Fix abridgeScore

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,7 @@ export function abridgeScore(score: number): string {
     return "0pt";
   }
   if (Math.abs(score) > 999) {
-    return (Math.sign(score) * (Math.abs(score) / 1000)).toFixed(1) + "pt";
+    return (Math.sign(score) * (Math.abs(score) / 1000)).toFixed(1) + "kpt";
   }
   return (Math.sign(score) * Math.abs(score)).toString() + "pt";
 }
