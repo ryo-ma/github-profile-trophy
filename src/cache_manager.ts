88f0ae731cf8dab8bc2c4c374f5a5b969b4c801a
Alex Oliveira <alexoliveira7x@gmail.com>
2023-09-25 21:54:24 +0100

    chore: ignore read permission throw

--- src/cache_manager.ts
+++ src/cache_manager.ts
@@ -16,14 +16,8 @@ const existsSync = (filename: string): boolean => {
       Deno.statSync(filename);
       // successful, file or directory must exist
       return true;
-    } catch (error) {
-      if (error instanceof Deno.errors.NotFound) {
-        // file or directory does not exist
+    } catch {
         return false;
-      } else {
-        // unexpected error, maybe permissions, pass it along
-        throw error;
-      }
     }
   };
   
