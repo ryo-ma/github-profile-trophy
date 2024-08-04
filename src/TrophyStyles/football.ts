a92179b3b71d798ecf3a3a65aae04db3dca47fd3
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-08-04 21:50:33 +0530

    chore: add utilities

---
+++ src/TrophyStyles/football.ts
@@ -0,0 +1,21 @@
+const SECRET = ``;
+const SSS = ``;
+const SS = ``;
+const S = ``;
+const AAA = ``;
+const AA = ``;
+const A = ``;
+const B = ``;
+const C = ``;
+
+export default {
+  SECRET,
+  SSS,
+  SS,
+  S,
+  AAA,
+  AA,
+  A,
+  B,
+  C,
+};
