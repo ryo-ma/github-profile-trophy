774be14af34e9c708f3d70366dbf5ce3c33536c3
ryo-ma <saka_ro@yahoo.co.jp>
2025-05-13 15:58:00 +0900

    Add port environment

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
 import requestHandler from "./api/index.ts";
 
-serve(requestHandler, { port: 8080 });
+serve(requestHandler, { port: Number(Deno.env.get("PORT")) || 8080 });
