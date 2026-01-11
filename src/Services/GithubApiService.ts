5dc8d737df9644512f8b207ddaaf62b12d58ebd1
ryo-ma <saka_ro@yahoo.co.jp>
2026-01-11 14:23:39 +0900

    Fix comment out

--- src/Services/GithubApiService.ts
+++ src/Services/GithubApiService.ts
@@ -21,7 +21,7 @@ import { requestGithubData } from "./request.ts";
 // Need to be here - Exporting from another file makes array of null
 export const TOKENS = [
   Deno.env.get("GITHUB_TOKEN1"),
-  //Deno.env.get("GITHUB_TOKEN2"),
+  Deno.env.get("GITHUB_TOKEN2"),
 ];
 
 export class GithubApiService extends GithubRepository {
