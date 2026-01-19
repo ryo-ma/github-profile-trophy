97e9fad67971e2558d58a6284f368220cd8552a1
cromatikap <7074019+cromatikap@users.noreply.github.com>
2026-01-19 23:46:07 +0000

    Fix hardcoded url in form (#407)
    Co-authored-by: cromatikap <cromatikap@users.noreply.github.com>

--- api/index.ts
+++ api/index.ts
@@ -59,7 +59,7 @@ async function app(req: Request): Promise<Response> {
       <div>
         <h2>You can use this form: </h2>
         <p>Enter your username and click get trophies</p>
-        <form action="https://github-profile-trophy.vercel.app/" method="get">
+        <form action="${base}" method="get">
           <label for="username">GitHub Username</label>
           <input type="text" name="username" id="username" placeholder="Ex. gabriel-logan" required>
           <label for="theme">Theme (Optional)</label>
@@ -73,7 +73,6 @@ async function app(req: Request): Promise<Response> {
         </form>
       </div>
       <script>
-        const base = "https://github-profile-trophy.vercel.app/";
         const button = document.querySelector("button");
         const input = document.querySelector("input");
         const temporarySpan = document.querySelector("#temporary-span");
