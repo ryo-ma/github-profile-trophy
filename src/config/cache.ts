082b1fd9f07779c3443f172eca643134df4a724d
cromatikap <cromatikap@users.noreply.github.com>
2026-01-19 19:28:59 +0000

    Fix redis connection when no password is set

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -23,8 +23,8 @@ class CacheProvider {
     this.client = await connect({
       hostname: Deno.env.get("REDIS_HOST") || "",
       port: Number(Deno.env.get("REDIS_PORT")) || 6379,
-      username: Deno.env.get("REDIS_USERNAME") || "",
-      password: Deno.env.get("REDIS_PASSWORD") || "",
+      username: Deno.env.get("REDIS_USERNAME") || undefined,
+      password: Deno.env.get("REDIS_PASSWORD") || undefined,
     });
   }
 
